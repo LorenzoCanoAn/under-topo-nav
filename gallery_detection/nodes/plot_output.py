85697955e2dba28f96afb38b0358d30e82372858
Lorenzo Cano <lcano@unizar.es>
2022-05-24 18:25:39 +0200

    added tracking to launch file

--- gallery_detection/nodes/plot_output.py
+++ gallery_detection/nodes/plot_output.py
@@ -6,44 +6,45 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def callback(msg, angles):
-    assert isinstance(msg, Float32MultiArray)
-    plt.gca().clear()
-    raw_vector = np.array(msg.data)
-    inverted = np.flip(raw_vector)
-    shifted = np.roll(inverted, 180)
-    plt.plot(angles[0].flatten(), shifted.flatten())
-    plt.gca().set_theta_zero_location("N")
-    plt.gca().set_rlim([0, 1])
-    plt.draw()
+class Plotter:
+    def __init__(self):
+        self.angles = (np.arange(0, 360, 1) / 360 * 2 * np.math.pi).flatten()
+        self.x = np.zeros(0)
+        self.y = np.zeros(0)
+        rospy.Subscriber(
+            "/tracked_galleries",
+            Float32MultiArray,
+            callback=self.tracked_callback,
+        )
+        rospy.Subscriber(
+            "/gallery_detection_vector",
+            Float32MultiArray,
+            callback=self.callback,
+        )
 
+    def callback(self, msg):
+        assert isinstance(msg, Float32MultiArray)
+        plt.gca().clear()
+        raw_vector = np.array(msg.data)
+        inverted = np.flip(raw_vector)
+        shifted = np.roll(inverted, 180)
+        plt.plot(self.angles, shifted.flatten())
+        plt.scatter(self.x, self.y)
+        plt.gca().set_theta_zero_location("N")
+        plt.gca().set_rlim([0, 1])
+        plt.draw()
 
-def tracked_callback(msg):
-    assert isinstance(msg, Float32MultiArray)
-    x = np.array(msg.data)
-    y = np.ones(len(x))
-    plt.scatter(x, y)
-    plt.gca().set_theta_zero_location("N")
-    plt.gca().set_rlim([0, 1])
-    plt.draw()
+    def tracked_callback(self, msg):
+        assert isinstance(msg, Float32MultiArray)
+        self.x = np.array(msg.data)
+        self.y = np.ones(len(self.x))
 
 
 def main():
     fig = plt.figure()
     ax = fig.add_subplot(111, projection="polar")
-    angles = np.arange(0, 360, 1) / 360 * 2 * np.math.pi
     rospy.init_node("plotter")
-    rospy.Subscriber(
-        "/tracked_galleries",
-        Float32MultiArray,
-        callback=tracked_callback,
-    )
-    rospy.Subscriber(
-        "/gallery_detection_vector",
-        Float32MultiArray,
-        callback=callback,
-        callback_args=[angles],
-    )
+    plotter = Plotter()
     plt.show()
 
 
