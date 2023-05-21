d1e8aa8fa7480e62646506020c0ce8f18038b1c7
lcano@unizar.es <lcano@unizar.es>
2023-05-21 13:37:16 +0200

    "Added possibility to block the velocicity with a topic"

--- heading_control/scripts/angle_to_vel.py
+++ heading_control/scripts/angle_to_vel.py
@@ -9,38 +9,48 @@ MAX_VEL = 1
 MIN_DIST = 1
 
 
-def angle_to_speed(angle):
-    if angle > math.pi:
-        angle -= 2 * math.pi
-    w = angle
-    if abs(w) > MAX_ANG_VEL:
-        w = w / abs(w) * MAX_ANG_VEL
-    v = (MAX_VEL * (min(2, 2) / 2)) - abs(w) / MAX_ANG_VEL * MAX_VEL * 0.2
-    v = max((v, 0))
-    print((v, w))
-    return v, w
+class AngleToVelNode:
+    def __init__(self) -> None:
+        rospy.init_node("angle_to_twist")
+        self.publisher = rospy.Publisher("/cmd_vel", geometry_msgs.Twist, queue_size=10)
+        self.subscriber = rospy.Subscriber(
+            "/corrected_angle",
+            std_msgs.Float32,
+            callback=self.angle_callback,
+        )
+        self.obstacle_sub = rospy.Subscriber(
+            "obstacle_detected", std_msgs.Bool, callback=self.obstacle_detected_callback
+        )
+        self.obstacle_detected = False
 
+    def obstacle_detected_callback(self, msg: std_msgs.Bool):
+        self.obstacle_detected = msg.data
 
-def angle_callback(msg, publisher):
-    publisher = publisher[0]
-    angle = msg.data
-    print(angle)
-    v, w = angle_to_speed(angle)
-    out_msg = geometry_msgs.Twist()
-    out_msg.angular.z = w
-    out_msg.linear.x = v
-    publisher.publish(out_msg)
+    def angle_to_speed(self, angle):
+        if angle > math.pi:
+            angle -= 2 * math.pi
+        w = angle
+        if abs(w) > MAX_ANG_VEL:
+            w = w / abs(w) * MAX_ANG_VEL
+        v = (MAX_VEL * (min(2, 2) / 2)) - abs(w) / MAX_ANG_VEL * MAX_VEL * 0.2
+        v = max((v, 0))
+        print((v, w))
+        return v, w
+
+    def angle_callback(self, msg):
+        if self.obstacle_detected:
+            self.publisher.publish(geometry_msgs.Twist())
+        else:
+            angle = msg.data
+            v, w = self.angle_to_speed(angle)
+            out_msg = geometry_msgs.Twist()
+            out_msg.angular.z = w
+            out_msg.linear.x = v
+            self.publisher.publish(out_msg)
 
 
 def main():
-    rospy.init_node("angle_to_twist")
-    publisher = rospy.Publisher("/cmd_vel", geometry_msgs.Twist, queue_size=10)
-    subscriber = rospy.Subscriber(
-        "/corrected_angle",
-        std_msgs.Float32,
-        callback=angle_callback,
-        callback_args=[publisher],
-    )
+    node = AngleToVelNode()
     rospy.spin()
 
 
